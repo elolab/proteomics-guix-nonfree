fc5b1a29a9c84d21cecb07c2e8b6b845aa4632bb
js <>
2024-10-07 14:02:02 +0300

    add license information
    * diaumpire.scm:

--- unelo-proteomics-nonfree/packages/diaumpire.scm
+++ unelo-proteomics-nonfree/packages/diaumpire.scm
@@ -15,6 +15,21 @@
 ;; that haven't been detangled yet.
 ;; https://guix.gnu.org/blog/2019/reproducible-builds-summit-5th-edition/
 ;; https://framagit.org/tyreunom/guix-more/-/tree/master
+;; Copyright (C) 2024
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-public diaumpire-se
   (package
     (name "diaumpire-se")
